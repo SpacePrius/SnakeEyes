253da34178c1477bbb0287958c13288fb82a8baa
Emily Stringer <spaceprius@gmail.com>
2021-02-19 20:11:14 -0500

    adding more tests and modifying readme

--- tests/dice_logic_test.py
+++ tests/dice_logic_test.py
@@ -15,9 +15,11 @@ class TestMath():
         return snakeeyes.Roll("1d20/5")
     def test_raw(self):
         return snakeeyes.Roll("25*3")
+    def test_parenth(self):
+        return snakeeyes.Roll("(1d20+5)*20")
 
 class TestOperators():
     def test_success(self):
         return snakeeyes.Roll("1d20>5")
     def test_explode(self):
-        return snakeeyes.Roll("1000d20x10")
+        return snakeeyes.Roll("100d20x10")
