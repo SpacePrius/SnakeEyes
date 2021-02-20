7d2f70633473aa0cc5966b143ea4ec23b8f3d3e0
Emily Stringer <spaceprius@gmail.com>
2021-02-19 20:24:01 -0500

    setup changes

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="snakeeyes-spaceprius",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="Emily Stringer",
     author_email="emily@spaceprius.com",
-    description="A small example package",
+    description="A simple python dice library built with regex, currently in alpha",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SpacePrius/SnakeEyes",
